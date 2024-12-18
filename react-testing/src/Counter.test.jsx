96b1045783379d9f2ddb2c9c1445ce1207f07a31
Franklin Kong <franklinkong981@gmail.com>
2024-12-18 10:44:36 -0800

    Finished testing demo

--- react-testing/src/Counter.test.jsx
+++ react-testing/src/Counter.test.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import {render, screen} from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import Counter from "./Counter";
+import { expect } from "vitest";
 
 test("it renders without crashing", () => {
   render(<Counter />);
 });
 
-test("playing with queries", () => {
-  const {getAllByText, getByText, queryByText, getByPlaceholderText} = render(<Counter/>);
-  console.log(getByPlaceholderText('username'));
+test('button increments counter', () => {
+  const {getByText, debug} = render(<Counter />);
+  debug();
+  //initial state of the counter
+  const h2 = getByText('Current count: 0');
+
+  const btn = getByText("Add");
+  fireEvent.click(btn);
+  debug();
+  expect(h2).toHaveTextContent('1');
+  expect(h2).not.toHaveTextContent('0');
 });
+
--- react-testing/src/Toggler.test.jsx
+++ react-testing/src/Toggler.test.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
 import Toggler from './Toggler.jsx';
+import { expect } from 'vitest';
 
-it('should start showing the h1', () => {
+it('should toggle', () => {
   const {getByText} = render(<Toggler/>);
   const heading = getByText("Hello World");
   expect(heading).toHaveClass('Toggler-text');
   expect(heading).toBeInTheDocument();
+
+  //click the button the first time and now we expect the heading to not be in there.
+  fireEvent.click(getByText('Toggle'));
+  expect(heading).not.toBeInTheDocument();
 });
